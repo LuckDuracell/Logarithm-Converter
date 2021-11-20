4412df1bf2031193b103fa26fac8722b8778e88d
LuckDuracell <lukedru@yahoo.com>
2021-11-19 23:36:38 -0800

    Initial Commit

---
+++ Logarithm Converter/Logarithm_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Logarithm_ConverterApp.swift
+//  Logarithm Converter
+//
+//  Created by Luke Drushell on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Logarithm_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
